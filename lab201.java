c8b9662e9d6426a6497024d8e04b780d6165c48f
Angsupantharawon <96603886+Augsupa@users.noreply.github.com>
2022-12-06 23:03:57 +0700

    Add files via upload

---
+++ lab201.java
@@ -0,0 +1,28 @@
+package LAB222;
+
+import java.util.Scanner;
+
+public class lab2 {
+    public static void main(String[] args) {
+    	int Number ;
+    	
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Please enter your value : ");
+        
+        Number = myObj.nextInt();  // Read user input
+        System.out.println("-----------------------");
+
+
+    	for(int i=1; i<=Number; i++ ) {
+    		System.out.print(i);
+    		
+    		if(i%5==0) {
+    			System.out.println("");
+    		}else {
+    			System.out.print(" ,");
+    		}
+    		
+    	}
+    	        
+    }
+}
